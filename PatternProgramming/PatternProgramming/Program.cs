c08ca24ba4d313dbaaf44e3d06c083617df6e09b
Rakib Islam <rakib.shehab@gmail.com>
2020-01-19 19:49:15 +0600

    first commit

---
+++ PatternProgramming/PatternProgramming/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatternProgramming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n=5, row, col;
+            
+            for (row = 1; row <= n; row++)
+            {
+                for (col = 1; col <= row; col++)
+                {
+                    Console.Write(col);
+                }
+
+                Console.WriteLine("\n");
+            }
+            Console.ReadKey();
+        }
+    }
+}
