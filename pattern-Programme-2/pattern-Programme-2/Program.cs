1e93b922ef77028e7f37a2c3792c5fc557d9f1f0
Rakib Islam <rakib.shehab@gmail.com>
2020-01-21 21:54:43 +0600

    commit

---
+++ pattern-Programme-2/pattern-Programme-2/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pattern_Programme_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Pattern Programming-1
+            //1 2 3 4 5 
+            //1 2 3 4
+            //1 2 3 
+            //1 2
+            //1
+            /*
+            int n = 5, row, col;
+
+            for (row = n; row>=1; row--)
+            {
+                for (col = row; col >= 1; col--)
+                {
+                    Console.Write(col);
+                }
+
+                Console.WriteLine("\n");
+            }
+            Console.ReadKey();
+            */
+            //Pattern Programming-1
+            //5 5 5 5 5 
+            //4 4 4 4
+            //3 3 3 
+            //2 2
+            //1
+            /*
+            int n = 5, row, col;
+
+            for (row = n; row >= 1; row--)
+            {
+                for (col = row; col >= 1; col--)
+                {
+                    Console.Write(row);
+                }
+
+                Console.WriteLine("\n");
+            }
+            Console.ReadKey();
+            */
+        }
+    }
+}
